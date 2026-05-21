c97da5c7028bfd7f6c7e6fe0dfea5370848cd3bc
Elias Wasem <ewasem@outlook.de>
2026-05-21 09:32:23 +0200

    Hopefully Fixed packet_handler.cpp

--- mibo-firmware/src/logic/communication/packet_handler.cpp
+++ mibo-firmware/src/logic/communication/packet_handler.cpp
@@ -9,9 +9,11 @@ uint8_t PacketHandler::m_payloadBuffer[256] = {0};
 PacketHandler::RxState PacketHandler::m_rxState = PacketHandler::RxState::WAIT_START;
 uint8_t PacketHandler::m_bytesToRead = 0;
 uint8_t PacketHandler::m_bytesRead = 0;
+Packet PacketHandler::m_currentCmd = static_cast<Packet>(0);
 
 void PacketHandler::parseByte(uint8_t byte) {
     switch (m_rxState) {
+
         case RxState::WAIT_START:
             if (byte == PROTOCOL_START) {
                 m_rxState = RxState::READ_LEN;
@@ -19,12 +21,26 @@ void PacketHandler::parseByte(uint8_t byte) {
             break;
 
         case RxState::READ_LEN:
-            if (byte >= 1) {
-                m_bytesToRead = byte;
-                m_bytesRead = 0;
-                m_rxState = RxState::READ_PAYLOAD;
-            } else {
+            m_bytesToRead = byte;
+            m_bytesRead = 0;
+
+            m_rxState = RxState::READ_CMD;
+            break;
+
+        case RxState::READ_CMD:
+            m_currentCmd = static_cast<Packet>(byte);
+
+            if (m_bytesToRead == 0) {
+                RxPacket packet;
+                packet.cmd = m_currentCmd;
+                packet.data = nullptr;
+                packet.dataLen = 0;
+
+                AppController::onPacketReceived(packet);
+
                 m_rxState = RxState::WAIT_START;
+            } else {
+                m_rxState = RxState::READ_PAYLOAD;
             }
             break;
 
@@ -33,9 +49,9 @@ void PacketHandler::parseByte(uint8_t byte) {
 
             if (m_bytesRead == m_bytesToRead) {
                 RxPacket packet;
-                packet.cmd = static_cast<Packet>(m_payloadBuffer[0]);
-                packet.data = &m_payloadBuffer[1];
-                packet.dataLen = m_bytesToRead - 1;
+                packet.cmd = m_currentCmd;
+                packet.data = m_payloadBuffer;
+                packet.dataLen = m_bytesToRead;
 
                 AppController::onPacketReceived(packet);
 
